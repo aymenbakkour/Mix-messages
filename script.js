821e02a5548585d198176481ac9734a57d8894fa
aymenbakkour <76061720+aymenbakkour@users.noreply.github.com>
2023-01-01 18:25:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Code to generator messages
+let i = Math.floor(Math.random() * 10);
+  
+    switch (i) {
+        case  1 :
+            window.alert("حكمة اليوم: من جد وجد");
+            break;
+        case  1:
+            window.alert("حكمة اليوم:  العلم نور");
+            break;
+        case  2:
+            window.alert("حكمة اليوم: خيرجليس في الأنام كتاب");
+            break;
+        case  3:
+            window.alert("حكمة اليوم:  الجار قبل الدار");
+            break;
+        case  4:
+            window.alert("حكمة اليوم:  من زاد في حبه لنفسه زاد كره الناس له");
+            break;
+        case  5:
+            window.alert("حكمة اليوم:  كُن عادلاً قبل أن تكون كريماً.");
+            break;
+        case  6:
+            window.alert("حكمة اليوم:  يسخر من الجروح كل من لا يعرف الألم");
+            break;
+        case  6:
+            window.alert("حكمة اليوم: لسان العاقل وراء قلبه، وقلب الأحمق وراء لسانه");
+            break;
+        case  6:
+            window.alert("حكمة اليوم:  من اتكل في سفره على زاد غيره طال جوعه.");
+            break;
+        case  6:
+            window.alert("حكمة اليوم: الابتسامة كلمة طيبة بغير حروف");
+            break;
+        default:
+            window.alert("حكمة اليوم:  اطلب العلم ولو في الصين");
+            break;
+}
+        
+
